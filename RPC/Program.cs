7166f6ceb2b76880814a9ac9a85e2706de463ccd
JAVIER De castro <javicas2005@gmail.com>
2021-10-06 21:36:37 +0200

    V0.6 Funcional? sin inventario

--- RPC/Program.cs
+++ RPC/Program.cs
@@ -58,8 +58,12 @@ namespace RPC
 
             }
         Creditos:
-            Console.WriteLine("No Disponible por el momento");
+            Console.WriteLine("Hecho por Jandriv");
             System.Threading.Thread.Sleep(800);
+            Console.WriteLine("...");
+            System.Threading.Thread.Sleep(800);
+            Console.WriteLine("Eso es todo");
+            System.Threading.Thread.Sleep(1400);
             Console.Clear();
             goto Titulo;
         Jugar:
@@ -73,18 +77,27 @@ namespace RPC
                     VidaPersonaje = 50; AtaquePersonaje = 75; DefensaPersonaje = 25; VelocidadPersonaje = 20;
                     ClasePersonaje = "Mago";
                     break;
+
                 case "b":
                     VidaPersonaje = 50; AtaquePersonaje = 25; DefensaPersonaje = 75; VelocidadPersonaje = 20;
                     ClasePersonaje = "Caballero";
                     break;
+
                 case "c":
                     VidaPersonaje = 25; AtaquePersonaje = 50; DefensaPersonaje = 50; VelocidadPersonaje = 40;
                     ClasePersonaje = "Cazador";
                     break;
+
                 case "d":
                     VidaPersonaje = 75; AtaquePersonaje = 50; DefensaPersonaje = 50; VelocidadPersonaje = 10;
                     ClasePersonaje = "Luchador";
                     break;
+
+                case "debug":
+                    VidaPersonaje = 750; AtaquePersonaje = 500; DefensaPersonaje = 500; VelocidadPersonaje = 100;
+                    ClasePersonaje = "Made in heaven";
+                    break;
+
                 default:
                     Console.Clear();
                     goto Jugar;
@@ -133,39 +146,69 @@ namespace RPC
                     VelocidadEnemigo = RandomNumber(VelocidadMinEnemigo, VelocidadMaxEnemigo);
                     DefensaEnemigo = RandomNumber(DefensaMinEnemigo, DefensaMaxEnemigo);
                 }
-                while (VidaEnemigo >= 0 || VidaPersonaje >= 0)
+                while (VidaEnemigo > 0 && VidaPersonaje > 0)
                 {
                     Console.Clear();
                     Console.WriteLine("Piso: " + i + "\n\n\n");
                     Console.WriteLine("¿Que quieres hacer?");
-                    Console.WriteLine("\n\ta - Luchar\n\tb - Estadísticas\n\tc - Inventario\n\td - Huir");
+                    Console.WriteLine("\n\ta - Luchar\n\tb - Bloquear\n\tc - Estadísticas\n\td - Inventario\n\te - Huir");
                     switch (Console.ReadLine())
                     {
                         case "a":
-                            Daño = 10;
                             Console.WriteLine(NombrePersonaje + " atacó al enemigo");
-                            Daño = ((AtaquePersonaje * DefensaEnemigo * RandomNumber(15, 36)) / 1000);
                             System.Threading.Thread.Sleep(1200);
-                            Console.WriteLine("El ataque hizo " + Daño + " puntos de daño");
-                            Vida = VidaEnemigo;
-                            VidaEnemigo = (Vida - (Daño));
-                            System.Threading.Thread.Sleep(1200);
-                            Console.WriteLine("¡El enemigo atacó de vuelta!");
-                            Vida = VidaPersonaje;
-                            Daño = ((AtaqueEnemigo * DefensaPersonaje * RandomNumber(10, 31)) / 1000);
-                            VidaPersonaje = (Vida - (Daño));
-                            System.Threading.Thread.Sleep(1200);
-                            Console.WriteLine("El ataque hizo " + Daño + " puntos de daño");
-                            Console.ReadKey();
+                            if (RandomNumber(1, 101) >= VelocidadEnemigo)
+                            {
+                                Daño = Convert.ToInt32(AtaquePersonaje * (1 - ((double)DefensaEnemigo / 100)) * (double)RandomNumber(85, 116) / 100);
+                                Console.WriteLine("El ataque hizo " + Daño + " puntos de daño");
+                                Vida = VidaEnemigo;
+                                VidaEnemigo = (Vida - (Daño));
+                            }
+                            else
+                            {
+                                Console.WriteLine("¡El enemigo esquivo el ataque!");
+                                System.Threading.Thread.Sleep(1200);
+                            }
+                            if (EnemigoRecarga = false)
+                            {
+                                System.Threading.Thread.Sleep(1200);
+                                Console.WriteLine("¡El enemigo atacó de vuelta!");
+                                if (RandomNumber(1, 101) >= VelocidadPersonaje)
+                                {
+                                    Vida = VidaPersonaje;
+                                    Daño = Convert.ToInt32(AtaqueEnemigo * ((1 - (double)(DefensaPersonaje / 100))) * (double)RandomNumber(85, 116) / 100);
+                                    VidaPersonaje = (Vida - (Daño));
+                                    System.Threading.Thread.Sleep(1200);
+                                    Console.WriteLine("El ataque hizo " + Daño + " puntos de daño");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Esquivaste el ataque");
+                                }
+                                Console.ReadKey();
+                            }
+                            else
+                            {
+                                Console.WriteLine("El enemigo esta recargando su ataque");
+                                EnemigoRecarga = false;
+                                Console.ReadKey();
+                            }
                             break;
 
                         case "b":
+                            Daño = Convert.ToInt32(AtaqueEnemigo * (1 - ((double)DefensaPersonaje / 100)) * (double)RandomNumber(25, 46) / 100);
+                            Vida = VidaPersonaje;
+                            VidaPersonaje = (Vida - (Daño));
+                            Console.WriteLine("Bloqueaste el ataque y recibiste " + Daño + " puntos de daño");
+                            System.Threading.Thread.Sleep(1200);
+                            break;
+                        case "c":
                             Console.Clear();
                             Console.WriteLine("Estadísticas del enemigo ");
                             Console.WriteLine("---------------------------------------");
                             Console.WriteLine("Vida: " + VidaEnemigo);
-                            Console.WriteLine("Defensa: " + DefensaEnemigo);
                             Console.WriteLine("Ataque: " + AtaqueEnemigo);
+                            Console.WriteLine("Defensa: " + DefensaEnemigo);
                             Console.WriteLine("Velocidad: " + VelocidadEnemigo);
                             Console.WriteLine("\n\nEstadísticas de " + NombrePersonaje);
                             Console.WriteLine("---------------------------------------");
@@ -177,14 +220,14 @@ namespace RPC
                             Console.Clear();
                             break;
 
-                        case "c":
+                        case "d":
                             Console.Clear();
                             Console.WriteLine("En progreso");
                             System.Threading.Thread.Sleep(800);
                             Console.Clear();
                             break;
 
-                        case "d":
+                        case "e":
                             Console.Clear();
                             Console.WriteLine("...");
                             System.Threading.Thread.Sleep(800);
@@ -209,6 +252,19 @@ namespace RPC
                             }
                             break;
                     }
+                }
+                if (VidaEnemigo <= 0)
+                {
+                    Console.WriteLine("Enemigo derrotado");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Has muerto");
+                    Console.ReadKey();
+                    Console.Clear();
+                    goto Titulo;
                 }
 
             FinDelPiso:;
